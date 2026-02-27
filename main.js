3d3ac284a12bce2abb41b9a119ca27f758e04c39
sky20425 <sky20425@gmail.com>
2026-02-27 21:08:47 +0000

    Update project files

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-// Add JS here
+const generatorBtn = document.getElementById('generator-btn');
+const numberSpans = document.querySelectorAll('.number');
+
+generatorBtn.addEventListener('click', () => {
+    const lottoNumbers = [];
+    while (lottoNumbers.length < 6) {
+        const randomNumber = Math.floor(Math.random() * 45) + 1;
+        if (!lottoNumbers.includes(randomNumber)) {
+            lottoNumbers.push(randomNumber);
+        }
+    }
+
+    numberSpans.forEach((span, index) => {
+        span.textContent = lottoNumbers[index];
+    });
+});
